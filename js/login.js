109018c775678d4069f52d369d9609b2fbaae7c9
MarianaSDiaz <maruuus.95@gmail.com>
2021-04-06 20:30:10 -0300

    Js login corrected

--- js/login.js
+++ js/login.js
@@ -14,10 +14,57 @@ function formCheck() {
     // if (formElements.length != 3) {
     //     errorArray.push("form is not complete");
     // }; 
-    console.log(errorArray);
+    // console.log(errorArray);
+};
+function checkLabelEmail() {
+    const labelEmail = document.querySelector(['label[for="email"]']);
+    if(!labelEmail) {
+        errorsArray.push('Email label is missing');
+    }
 };
 
+function checkInputEmail() {
+    const inputEmail = document.querySelector(['input[type="email"][name=email][required]']);
+    if(!inputEmail) {
+        errorsArray.push('E-mail input is missing or incorrect');
+    }
+};
 
+function checkLabelPassword() {
+    const labelPassword = document.querySelector(['label[for="password"]']);
+    if(!labelPassword) {
+        errorsArray.push('Password label is missing');
+    }
+};
+
+function checkInputPassword() {
+    const inputPassword = document.querySelector(['input[type="password"][name="passw"][required]']);
+    if(!inputPassword) {
+        errorArray.push('Password input is missing or incorrect');
+    }
+};
+
+function checkLoginButton() {
+    const loginButton = document.querySelector(['button[type="submit"]']);
+    if(!loginButton) {
+        errorsArray.push('Missing submit button');
+    }
+};
+
+function check(){
+    const ValidationsResults = document.getElementsByClassName("validationContent");
+    if(errorArray.length === 0) {
+        ValidationsResults.innerHTML += '<p>Every validation has passed</p>';
+    } else {
+        for (let i = 0; i < errorArray.length; i++) {
+            console.log(errorsArray[i]);
+            ValidationsResults.innerHTML += '<p>' + errorArray[i] + '</p>';
+        }
+    }
+};
 formCheck();
-
-
+checkLabelEmail();
+checkInputEmail();
+checkLabelPassword();
+checkInputPassword();
+check();
